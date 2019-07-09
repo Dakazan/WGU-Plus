9c54c9abf4647d35ecc3e0bac788ff73e3888c9c
Dakazan <rtur112@wgu.edu>
2019-07-09 13:53:40 -0600

    Change Chrome API to Firefox

--- src/background.js
+++ src/background.js
@@ -1,12 +1,12 @@
 // Whenever a new tab is created, try and add the Quick Links accordion
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+browser.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.url.startsWith("https://my.wgu.edu/courses/course/")) {
-    chrome.tabs.sendMessage(tabId, {type: 'getQuickLinks'});
+    browser.tabs.sendMessage(tabId, {type: 'getQuickLinks'});
   }
 });
 
 // If the Extension Icon is clicked, bring the user to the GitHub page.
-chrome.browserAction.onClicked.addListener(function(tab) {
+browser.browserAction.onClicked.addListener(function(tab) {
   var win = window.open("https://github.com/bamhm182/WGU-Plus", "_blank");
   win.focus();
 });
